0ffbf46570afefd0e73c5f4d6475908612fdbfcf
Bilgee Ganduuren <bilgeehatesyou@Bilgees-MacBook-Pro.local>
2025-09-29 16:23:02 +0800

    msg

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { connectDB } = require('./db');
 const PORT = 3000;
-categoriesRoutes = require('./routes/categories');
+const categoriesRoutes = require('./routes/categories');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
 app.use(express.json());
 app.use('/api/categories', categoriesRoutes);
+app.use('/api/auth', authRoutes);
 
 (async () => {
     await connectDB();
